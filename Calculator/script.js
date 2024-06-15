0265ebbbf065a8e20405401c3847417f791b208d
isimrangupta <simrandgupta95@gmail.com>
2024-06-15 19:05:35 +0530

    creating a calculator using JS

---
+++ Calculator/script.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('.inputbox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach(btn =>{
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string)
+            input.value = string;
+        } 
+        else if(e.target.innerHTML == "AC"){
+            string = "";
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == "DEL"){
+            string = string.slice(0, -1)
+            input.value = string;
+        
+        }
+        else {
+            string = string + e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
