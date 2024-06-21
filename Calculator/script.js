f76630f3a8c906c93b0329bc617d4ab4c897e619
isimrangupta <simrandgupta95@gmail.com>
2024-06-21 17:08:17 +0530

    Added some digits

--- Calculator/script.js
+++ Calculator/script.js
@@ -18,7 +18,6 @@ arr.forEach(btn =>{
         else if(e.target.innerHTML == "DEL"){
             string = string.slice(0, -1)
             input.value = string;
-        
         }
         else {
             string = string + e.target.innerHTML;
